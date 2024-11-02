15e816b9baab6ddb3d5594f557432e3a648f9501
I Putu Saputrayana <ikhyan7@gmail.com>
2024-11-03 00:09:00 +0800

    feat: add options

--- apps/storybook/src/stories/transfer-button/transfer-button.tsx
+++ apps/storybook/src/stories/transfer-button/transfer-button.tsx
@@ -1,25 +1,35 @@
 import { Button } from "@/components/ui/button";
+import { MutationOptions } from "@tanstack/react-query";
+import { Config, SendTransactionErrorType } from "@wagmi/core";
 import { Account, parseEther } from "viem";
 import { useSendTransaction } from "wagmi";
+import { SendTransactionData, SendTransactionVariables } from "wagmi/query";
 
-interface TransferButtonProps {
+type TransferButtonProps = {
   /** Amount to transfer in ETH */
   amount: number;
   /** Address to transfer to */
   to: string;
   /** Account to use for the transfer */
   account?: Account;
-}
+} & MutationOptions<
+  SendTransactionData,
+  SendTransactionErrorType,
+  SendTransactionVariables<Config, Config["chains"][number]["id"]>
+>;
 
-export function TransferButton({ amount, to, account }: TransferButtonProps) {
+export function TransferButton({ amount, to, account, ...mutationOptions }: TransferButtonProps) {
   const { sendTransaction, isPending } = useSendTransaction();
 
   const handleTransfer = () => {
-    sendTransaction({
-      to: to as `0x${string}`,
-      value: parseEther(amount.toString()),
-      account,
-    });
+    sendTransaction(
+      {
+        to: to as `0x${string}`,
+        value: parseEther(amount.toString()),
+        account,
+      },
+      mutationOptions
+    );
   };
 
   return (
