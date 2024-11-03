96eb3a09b729b884e80ffd48d4ab80c8d36f7b73
metalboyrick <rsulisthio@gmail.com>
2024-11-03 23:02:30 +0700

    test(tokenchip): add tokenchip tests and update vitest config to support DOM

--- apps/storybook/vite.config.ts
+++ apps/storybook/vite.config.ts
@@ -1,6 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+/// <reference types="vitest" />
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 import path from "path";
+import { test } from "vitest";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -9,5 +11,10 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
+  test: {
+    globals: true,
+    environment: "jsdom",
+    setupFiles: "./test-setup.ts",
+  },
   plugins: [react()],
-})
+});
