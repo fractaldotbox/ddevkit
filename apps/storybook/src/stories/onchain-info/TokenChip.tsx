49a6c9c9e2c5344eb078ba0ddc727b7b6baecefb
metalboyrick <rsulisthio@gmail.com>
2024-11-03 23:06:29 +0700

    chore(tokenchip): run biome

--- apps/storybook/src/stories/onchain-info/TokenChip.tsx
+++ apps/storybook/src/stories/onchain-info/TokenChip.tsx
@@ -1,22 +1,22 @@
 import { Button } from "@/components/ui/button";
 
 export type TokenChipProps = {
-  image?: string;
-  name: string;
-  symbol: string;
-  className?: string;
+	image?: string;
+	name: string;
+	symbol: string;
+	className?: string;
 };
 
 export const TokenChip = ({
-  image,
-  name,
-  symbol,
-  className,
+	image,
+	name,
+	symbol,
+	className,
 }: TokenChipProps) => {
-  return (
-    <Button variant={"secondary"} className={`py-1 flex gap-3 ${className}`}>
-      <img className="h-6" src={image} alt={`${name}-icon`} />
-      <div className="text-lg font-semibold">{symbol}</div>
-    </Button>
-  );
+	return (
+		<Button variant={"secondary"} className={`py-1 flex gap-3 ${className}`}>
+			<img className="h-6" src={image} alt={`${name}-icon`} />
+			<div className="text-lg font-semibold">{symbol}</div>
+		</Button>
+	);
 };
--- apps/storybook/vite.config.ts
+++ apps/storybook/vite.config.ts
@@ -1,20 +1,20 @@
+import path from "path";
+import react from "@vitejs/plugin-react";
 /// <reference types="vitest" />
 import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import path from "path";
 import { test } from "vitest";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
-  },
-  test: {
-    globals: true,
-    environment: "jsdom",
-    setupFiles: "./test-setup.ts",
-  },
-  plugins: [react()],
+	resolve: {
+		alias: {
+			"@": path.resolve(__dirname, "./src"),
+		},
+	},
+	test: {
+		globals: true,
+		environment: "jsdom",
+		setupFiles: "./test-setup.ts",
+	},
+	plugins: [react()],
 });
