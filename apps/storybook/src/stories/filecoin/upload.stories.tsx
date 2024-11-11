684dcfae82a08c48a32c16026a6e243cf15b9011
I Putu Saputrayana <ikhyan7@gmail.com>
2024-11-11 11:01:07 +0800

    fix: fix linter

--- apps/storybook/src/stories/filecoin/upload.stories.tsx
+++ apps/storybook/src/stories/filecoin/upload.stories.tsx
@@ -3,15 +3,15 @@ import type { Meta, StoryObj } from "@storybook/react";
 import { UploadForm } from "./upload-form";
 
 const meta = {
-    title: "Filecoin/UploadForm",
-    component: UploadForm,
-    argTypes: {},
-    args: {},
+	title: "Filecoin/UploadForm",
+	component: UploadForm,
+	argTypes: {},
+	args: {},
 } satisfies Meta<typeof UploadForm>;
 
 export default meta;
 type Story = StoryObj<typeof meta>;
 
 export const Basic: Story = {
-    args: {},
+	args: {},
 };
--- apps/storybook/src/stories/transfer-button/transfer-button.tsx
+++ apps/storybook/src/stories/transfer-button/transfer-button.tsx
@@ -7,44 +7,49 @@ import { useSendTransaction } from "wagmi";
 import { SendTransactionData, SendTransactionVariables } from "wagmi/query";
 
 type TransferButtonProps = {
-  /** Amount to transfer in ETH */
-  amount: number;
-  /** Address to transfer to */
-  to: string;
-  /** Account to use for the transfer */
-  account?: Account;
-  /** Viem mutation options */
-  mutationOptions?: MutationOptions<
-    SendTransactionData,
-    SendTransactionErrorType,
-    SendTransactionVariables<Config, Config["chains"][number]["id"]>
-  >;
+	/** Amount to transfer in ETH */
+	amount: number;
+	/** Address to transfer to */
+	to: string;
+	/** Account to use for the transfer */
+	account?: Account;
+	/** Viem mutation options */
+	mutationOptions?: MutationOptions<
+		SendTransactionData,
+		SendTransactionErrorType,
+		SendTransactionVariables<Config, Config["chains"][number]["id"]>
+	>;
 } & ButtonProps;
 
 export function TransferButton({
-  amount,
-  to,
-  account,
-  disabled,
-  mutationOptions,
-  ...buttonProps
+	amount,
+	to,
+	account,
+	disabled,
+	mutationOptions,
+	...buttonProps
 }: TransferButtonProps) {
-  const { sendTransaction, isPending } = useSendTransaction();
+	const { sendTransaction, isPending } = useSendTransaction();
 
-  const handleTransfer = () => {
-    sendTransaction(
-      {
-        to: to as `0x${string}`,
-        value: parseEther(amount.toString()),
-        account,
-      },
-      mutationOptions
-    );
-  };
+	const handleTransfer = () => {
+		sendTransaction(
+			{
+				to: to as `0x${string}`,
+				value: parseEther(amount.toString()),
+				account,
+			},
+			mutationOptions,
+		);
+	};
 
-  return (
-    <Button onClick={handleTransfer} disabled={isPending || disabled} {...buttonProps}>
-      {isPending && <Loader2 className="animate-spin w-4 h-4 mr-1" />} {account ? "Transfer" : "Connect Wallet"}
-    </Button>
-  );
+	return (
+		<Button
+			onClick={handleTransfer}
+			disabled={isPending || disabled}
+			{...buttonProps}
+		>
+			{isPending && <Loader2 className="animate-spin w-4 h-4 mr-1" />}{" "}
+			{account ? "Transfer" : "Connect Wallet"}
+		</Button>
+	);
 }
