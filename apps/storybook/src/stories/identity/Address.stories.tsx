b1d4a79be87dff55c743a71598af24a5ea71cefa
metalboyrick <rsulisthio@gmail.com>
2024-11-11 15:41:49 +0700

    chore(address): run biome

--- apps/storybook/src/stories/identity/Address.stories.tsx
+++ apps/storybook/src/stories/identity/Address.stories.tsx
@@ -1,29 +1,29 @@
-import type { Meta, StoryObj } from "@storybook/react";
 import { faker } from "@faker-js/faker";
-import { Address } from "./Address";
+import type { Meta, StoryObj } from "@storybook/react";
 import { Hex } from "viem";
+import { Address } from "./Address";
 
 const meta = {
-  title: "Identity/Address",
-  component: Address,
-  parameters: {
-    layout: "centered",
-  },
-  args: {},
+	title: "Identity/Address",
+	component: Address,
+	parameters: {
+		layout: "centered",
+	},
+	args: {},
 } satisfies Meta<typeof Address>;
 
 export default meta;
 type Story = StoryObj<typeof meta>;
 
 export const Short: Story = {
-  args: {
-    address: faker.finance.ethereumAddress() as Hex,
-  },
+	args: {
+		address: faker.finance.ethereumAddress() as Hex,
+	},
 };
 
 export const Full: Story = {
-  args: {
-    address: faker.finance.ethereumAddress() as Hex,
-    isFull: true,
-  },
+	args: {
+		address: faker.finance.ethereumAddress() as Hex,
+		isFull: true,
+	},
 };
