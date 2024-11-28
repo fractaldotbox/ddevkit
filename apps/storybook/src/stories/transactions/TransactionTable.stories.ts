621888c1176d6520fedf7bb4479b5d09bc903383
metalboyrick <rsulisthio@gmail.com>
2024-11-28 22:43:19 +0700

    feat(txn-table): add basic ui only

---
+++ apps/storybook/src/stories/transactions/TransactionTable.stories.ts
@@ -0,0 +1,24 @@
+import type { Meta, StoryObj } from "@storybook/react";
+import { mainnet } from "viem/chains";
+import { withWagmiProvider } from "../decorators/wagmi";
+import { TRANSACTION } from "../fixture";
+import { TransactionCard } from "./TransactionCard";
+import { TransactionTable } from "./TransactionTable";
+import { TXN_LIST } from "@/lib/blockscout/fixture";
+import { asTransactionMeta } from "@/lib/blockscout/api";
+
+const meta = {
+  title: "Transactions/TransactionTable",
+  component: TransactionTable,
+  args: {},
+} satisfies Meta<typeof TransactionTable>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const TransactionTableWithoutFetching: Story = {
+  args: {
+    transactions: TXN_LIST.map((txn) => asTransactionMeta(txn)),
+  },
+  decorators: [withWagmiProvider()],
+};
