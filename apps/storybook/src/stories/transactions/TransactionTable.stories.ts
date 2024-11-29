bf6789a455e187608f030375f0099cdc96b8e46a
metalboyrick <rsulisthio@gmail.com>
2024-11-29 22:43:23 +0700

    feat(txn-table): add faker fixture generation

--- apps/storybook/src/stories/transactions/TransactionTable.stories.ts
+++ apps/storybook/src/stories/transactions/TransactionTable.stories.ts
@@ -1,5 +1,5 @@
 import { asTransactionMeta } from "@/lib/blockscout/api";
-import { TXN_LIST } from "@/lib/blockscout/fixture";
+import { generateFixtures, TXN_LIST } from "@/lib/blockscout/fixture";
 import type { Meta, StoryObj } from "@storybook/react";
 import { withWagmiProvider } from "../decorators/wagmi";
 import { TransactionTable } from "./TransactionTable";
@@ -16,6 +16,13 @@ type Story = StoryObj<typeof meta>;
 export const TransactionTableWithoutFetching: Story = {
   args: {
     transactions: TXN_LIST.map((txn) => asTransactionMeta(txn)),
+  },
+  decorators: [withWagmiProvider()],
+};
+
+export const TransactionTableWithoutFetching2: Story = {
+  args: {
+    transactions: generateFixtures(50).map((txn) => asTransactionMeta(txn)),
   },
   decorators: [withWagmiProvider()],
 };
