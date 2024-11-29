f03ecc3348d75f2cd3896d10b1d3baecbcd2a32b
metalboyrick <rsulisthio@gmail.com>
2024-11-29 22:46:40 +0700

    chore(txn-table): run biome

--- apps/storybook/src/lib/blockscout/api.ts
+++ apps/storybook/src/lib/blockscout/api.ts
@@ -1,85 +1,85 @@
 import { Address, Transaction, parseGwei, parseUnits } from "viem";
 import {
-  TokenTransfer,
-  TransactionMeta,
+	TokenTransfer,
+	TransactionMeta,
 } from "../domain/transaction/transaction";
 
 const ROOT = "https://eth.blockscout.com/api/";
 export const invokeApi = async (endpoint: string, body?: any) => {
-  return fetch(endpoint, {
-    method: "GET",
-    headers: {
-      "Content-Type": "application/json",
-    },
-  }).then((res) => {
-    return res.json();
-  });
+	return fetch(endpoint, {
+		method: "GET",
+		headers: {
+			"Content-Type": "application/json",
+		},
+	}).then((res) => {
+		return res.json();
+	});
 };
 
 // TODO better pairing types
 export enum BlockscoutEndpoint {
-  Transaction = "transaction",
-  Address = "address",
+	Transaction = "transaction",
+	Address = "address",
 }
 export interface BlockscoutEndpointParams {
-  address?: Address;
-  txnHash?: string;
+	address?: Address;
+	txnHash?: string;
 }
 
 export const ENDPOINT_STRATEGIES = {
-  [BlockscoutEndpoint.Address]: (params: BlockscoutEndpointParams) => {
-    const { address } = params;
-    return ROOT + "v1/address/" + address;
-  },
-  [BlockscoutEndpoint.Transaction]: (params: BlockscoutEndpointParams) => {
-    const { txnHash } = params;
-    return ROOT + "v2/transactions/" + txnHash;
-  },
+	[BlockscoutEndpoint.Address]: (params: BlockscoutEndpointParams) => {
+		const { address } = params;
+		return ROOT + "v1/address/" + address;
+	},
+	[BlockscoutEndpoint.Transaction]: (params: BlockscoutEndpointParams) => {
+		const { txnHash } = params;
+		return ROOT + "v2/transactions/" + txnHash;
+	},
 };
 
 // TODO enum type
 export const getEndpoint = (
-  endpoint: BlockscoutEndpoint,
-  params: BlockscoutEndpointParams,
+	endpoint: BlockscoutEndpoint,
+	params: BlockscoutEndpointParams,
 ) => {
-  const strategy = ENDPOINT_STRATEGIES[endpoint];
-  if (!strategy) {
-    throw new Error("");
-  }
+	const strategy = ENDPOINT_STRATEGIES[endpoint];
+	if (!strategy) {
+		throw new Error("");
+	}
 
-  return strategy(params);
+	return strategy(params);
 };
 
 export const getAddressInfo = async (address: Address) => {
-  const endpoint = getEndpoint(BlockscoutEndpoint.Address, { address });
-  return invokeApi(endpoint);
+	const endpoint = getEndpoint(BlockscoutEndpoint.Address, { address });
+	return invokeApi(endpoint);
 };
 
 export const getTransaction = async (txnHash: string) => {
-  const endpoint = getEndpoint(BlockscoutEndpoint.Transaction, {
-    txnHash,
-  });
-  return invokeApi(endpoint);
+	const endpoint = getEndpoint(BlockscoutEndpoint.Transaction, {
+		txnHash,
+	});
+	return invokeApi(endpoint);
 };
 
 export const findDisplayedTxType = (transaction_types: any[]): string => {
-  if (transaction_types.includes("contract_call")) {
-    return "contract_call";
-  }
-  if (transaction_types.includes("coin_transfer")) {
-    return "coin_transfer";
-  }
-  return "native_transfer";
+	if (transaction_types.includes("contract_call")) {
+		return "contract_call";
+	}
+	if (transaction_types.includes("coin_transfer")) {
+		return "coin_transfer";
+	}
+	return "native_transfer";
 };
 
 export const asTokenTransfer = (transfer: any): TokenTransfer => {
-  const { token } = transfer;
-  return {
-    ...token,
-    // imageUrl: '',
-    name: transfer.token.name,
-    amount: parseUnits(transfer.total.value, transfer.total.decimals),
-  };
+	const { token } = transfer;
+	return {
+		...token,
+		// imageUrl: '',
+		name: transfer.token.name,
+		amount: parseUnits(transfer.total.value, transfer.total.decimals),
+	};
 };
 
 /**
@@ -88,16 +88,16 @@ export const asTokenTransfer = (transfer: any): TokenTransfer => {
  *
  */
 export const asTransactionMeta = (res: any): TransactionMeta => {
-  console.log(res);
-  return {
-    hash: res.hash,
-    blockHash: res.blockHash,
-    from: res.from.hash as Address,
-    to: res.to.hash as Address,
-    isSuccess: res.success,
-    displayedTxType: findDisplayedTxType(res.tx_types),
-    value: parseUnits(res.value, res.decimals),
-    tokenTransfers: res.token_transfers.map(asTokenTransfer),
-    gas: res.gas_used,
-  };
+	console.log(res);
+	return {
+		hash: res.hash,
+		blockHash: res.blockHash,
+		from: res.from.hash as Address,
+		to: res.to.hash as Address,
+		isSuccess: res.success,
+		displayedTxType: findDisplayedTxType(res.tx_types),
+		value: parseUnits(res.value, res.decimals),
+		tokenTransfers: res.token_transfers.map(asTokenTransfer),
+		gas: res.gas_used,
+	};
 };
--- apps/storybook/src/stories/transactions/TransactionTable.stories.ts
+++ apps/storybook/src/stories/transactions/TransactionTable.stories.ts
@@ -1,28 +1,28 @@
 import { asTransactionMeta } from "@/lib/blockscout/api";
-import { generateFixtures, TXN_LIST } from "@/lib/blockscout/fixture";
+import { TXN_LIST, generateFixtures } from "@/lib/blockscout/fixture";
 import type { Meta, StoryObj } from "@storybook/react";
 import { withWagmiProvider } from "../decorators/wagmi";
 import { TransactionTable } from "./TransactionTable";
 
 const meta = {
-  title: "Transactions/TransactionTable",
-  component: TransactionTable,
-  args: {},
+	title: "Transactions/TransactionTable",
+	component: TransactionTable,
+	args: {},
 } satisfies Meta<typeof TransactionTable>;
 
 export default meta;
 type Story = StoryObj<typeof meta>;
 
 export const TransactionTableWithoutFetching: Story = {
-  args: {
-    transactions: TXN_LIST.map((txn) => asTransactionMeta(txn)),
-  },
-  decorators: [withWagmiProvider()],
+	args: {
+		transactions: TXN_LIST.map((txn) => asTransactionMeta(txn)),
+	},
+	decorators: [withWagmiProvider()],
 };
 
 export const TransactionTableWithoutFetching2: Story = {
-  args: {
-    transactions: generateFixtures(50).map((txn) => asTransactionMeta(txn)),
-  },
-  decorators: [withWagmiProvider()],
+	args: {
+		transactions: generateFixtures(50).map((txn) => asTransactionMeta(txn)),
+	},
+	decorators: [withWagmiProvider()],
 };
