b81546d2193adaafc6844232c36b96da2ea65cb5
metalboyrick <rsulisthio@gmail.com>
2024-12-02 11:24:12 +0700

    refactor(txn-table): code cleanup for transaction table story

--- apps/storybook/src/stories/transactions/TransactionTable.stories.ts
+++ apps/storybook/src/stories/transactions/TransactionTable.stories.ts
@@ -5,24 +5,32 @@ import { withWagmiProvider } from "../decorators/wagmi";
 import { TransactionTable } from "./TransactionTable";
 
 const meta = {
-	title: "Transactions/TransactionTable",
-	component: TransactionTable,
-	args: {},
+  title: "Transactions/TransactionTable",
+  component: TransactionTable,
+  args: {},
+  parameters: {
+    layout: "centered",
+  },
+  decorators: [withWagmiProvider()],
 } satisfies Meta<typeof TransactionTable>;
 
 export default meta;
 type Story = StoryObj<typeof meta>;
 
 export const TransactionTableWithoutFetching: Story = {
-	args: {
-		transactions: TXN_LIST.map((txn) => asTransactionMeta(txn)),
-	},
-	decorators: [withWagmiProvider()],
+  args: {
+    transactions: TXN_LIST.map((txn) => asTransactionMeta(txn)),
+  },
 };
 
 export const TransactionTableWithoutFetching2: Story = {
-	args: {
-		transactions: generateFixtures(50).map((txn) => asTransactionMeta(txn)),
-	},
-	decorators: [withWagmiProvider()],
+  args: {
+    transactions: generateFixtures(50).map((txn) => asTransactionMeta(txn)),
+  },
+};
+
+export const TransactionTableWithBlockscoutAPI: Story = {
+  args: {
+    transactions: generateFixtures(50).map((txn) => asTransactionMeta(txn)),
+  },
 };
