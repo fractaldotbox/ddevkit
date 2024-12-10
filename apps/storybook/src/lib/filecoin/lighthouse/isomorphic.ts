9f88c1b690e0935df985772c756e00394b21698c
metalboyrick <rsulisthio@gmail.com>
2024-12-10 14:45:06 +0700

    chore(upload-attestation): run biome

--- apps/storybook/src/lib/filecoin/lighthouse/isomorphic.ts
+++ apps/storybook/src/lib/filecoin/lighthouse/isomorphic.ts
@@ -1,51 +1,51 @@
 import kavach from "@lighthouse-web3/kavach";
 import lighthouse from "@lighthouse-web3/sdk";
+import { IUploadProgressCallback } from "@lighthouse-web3/sdk/dist/types";
 import ky, { DownloadProgress } from "ky";
 import { http, Account, Hex, createWalletClient, custom } from "viem";
 import { sepolia } from "viem/chains";
 import { uploadFiles as uploadFilesLighthouse } from "./browser";
-import { IUploadProgressCallback } from "@lighthouse-web3/sdk/dist/types";
 // import { CID } from 'multiformats/cid'
 
 // Supposedly lighthouse can be treeshake for node/browser, to be validated
 
 export const LIGHTHOUSE_API_ROOT =
-  "https://api.lighthouse.storage/api/lighthouse/";
+	"https://api.lighthouse.storage/api/lighthouse/";
 
 // Consider model as action insteadz
 export const createLighthouseParams = async ({
-  account,
-  options,
+	account,
+	options,
 }: {
-  account: Account;
-  options: {
-    apiKey: string;
-  };
+	account: Account;
+	options: {
+		apiKey: string;
+	};
 }): Promise<[string, string, string]> => {
-  const { apiKey } = options;
+	const { apiKey } = options;
 
-  const signedMessage = await signAuthMessage(account);
-  return [apiKey, account.address, signedMessage];
+	const signedMessage = await signAuthMessage(account);
+	return [apiKey, account.address, signedMessage];
 };
 
 export const signAuthMessage = async (account: any) => {
-  const client = createWalletClient({
-    account,
-    chain: sepolia,
-    transport: custom(window.ethereum!),
-  });
+	const client = createWalletClient({
+		account,
+		chain: sepolia,
+		transport: custom(window.ethereum!),
+	});
 
-  const authMessage = await kavach.getAuthMessage(account.address);
+	const authMessage = await kavach.getAuthMessage(account.address);
 
-  const { error, message } = authMessage;
-  if (error || !message) {
-    throw new Error("authMessage error" + error);
-  }
+	const { error, message } = authMessage;
+	if (error || !message) {
+		throw new Error("authMessage error" + error);
+	}
 
-  return client.signMessage({
-    account,
-    message: message,
-  });
+	return client.signMessage({
+		account,
+		message: message,
+	});
 };
 
 // Api design issue cannot pass callback when deal params not specified
@@ -54,101 +54,101 @@ export const signAuthMessage = async (account: any) => {
 // consider direct invoke /api/v0/add?wrap-with-directory
 
 export const uploadFile = async (
-  file: File,
-  apiKey: string,
-  uploadProgressCallback?: (data: DownloadProgress) => void,
+	file: File,
+	apiKey: string,
+	uploadProgressCallback?: (data: DownloadProgress) => void,
 ): Promise<any> => {
-  let output;
+	let output;
 
-  if (window) {
-    output = await uploadFilesLighthouse<false>({
-      files: [file],
-      config: {
-        accessToken: apiKey,
-      },
-      uploadProgressCallback,
-    });
-  } else {
-    output = await lighthouse.upload(
-      file,
-      apiKey,
-      undefined,
-      (data: IUploadProgressCallback) => {
-        if (!uploadProgressCallback) return;
-        uploadProgressCallback({
-          percent: data.progress,
-          transferredBytes: 0,
-          totalBytes: 0,
-        });
-      },
-    );
-  }
+	if (window) {
+		output = await uploadFilesLighthouse<false>({
+			files: [file],
+			config: {
+				accessToken: apiKey,
+			},
+			uploadProgressCallback,
+		});
+	} else {
+		output = await lighthouse.upload(
+			file,
+			apiKey,
+			undefined,
+			(data: IUploadProgressCallback) => {
+				if (!uploadProgressCallback) return;
+				uploadProgressCallback({
+					percent: data.progress,
+					transferredBytes: 0,
+					totalBytes: 0,
+				});
+			},
+		);
+	}
 
-  if (!output?.data?.Hash) {
-    throw new Error("Upload failed");
-  }
+	if (!output?.data?.Hash) {
+		throw new Error("Upload failed");
+	}
 
-  return {
-    name: output.data.Name,
-    cid: output.data.Hash,
-    size: parseInt(output.data.Size, 10),
-  };
+	return {
+		name: output.data.Name,
+		cid: output.data.Hash,
+		size: parseInt(output.data.Size, 10),
+	};
 };
 
 export const retrievePoDsi = async (cid: string) => {
-  let response = await ky.get(`${LIGHTHOUSE_API_ROOT}/get_proof`, {
-    searchParams: {
-      cid,
-      network: "testnet", // Change the network to mainnet when ready
-    },
-  });
-  const data = await response.json();
-  return JSON.parse(data);
+	let response = await ky.get(`${LIGHTHOUSE_API_ROOT}/get_proof`, {
+		searchParams: {
+			cid,
+			network: "testnet", // Change the network to mainnet when ready
+		},
+	});
+	const data = await response.json();
+	return JSON.parse(data);
 };
 
 // .uploadText has no deal params options
 
 export const uploadText = async (text: string, apiKey: string) => {
-  if (!text) {
-    throw new Error("Empty text");
-  }
+	if (!text) {
+		throw new Error("Empty text");
+	}
 
-  const response = await lighthouse.uploadText(text, apiKey);
+	const response = await lighthouse.uploadText(text, apiKey);
 
-  const { data } = response;
+	const { data } = response;
 
-  return {
-    name: data.Name,
-    cid: data.Hash,
-    size: parseInt(data.Size, 10),
-  };
+	return {
+		name: data.Name,
+		cid: data.Hash,
+		size: parseInt(data.Size, 10),
+	};
 };
 
 export const uploadEncryptedFileWithText = async (
-  text: string,
-  apiKey: string,
-  publicKey: string,
-  signedMessage: string,
+	text: string,
+	apiKey: string,
+	publicKey: string,
+	signedMessage: string,
 ) => {
-  const response = await lighthouse.textUploadEncrypted(
-    text,
-    apiKey,
-    publicKey,
-    signedMessage,
-  );
+	const response = await lighthouse.textUploadEncrypted(
+		text,
+		apiKey,
+		publicKey,
+		signedMessage,
+	);
 
-  const { data } = response;
+	const { data } = response;
 
-  return {
-    name: data.Name,
-    cid: data.Hash,
-  };
+	return {
+		name: data.Name,
+		cid: data.Hash,
+	};
 };
 
 export const getLighthouseGatewayUrl = (cid: string) => {
-  return "https://gateway.lighthouse.storage/ipfs/" + cid;
+	return "https://gateway.lighthouse.storage/ipfs/" + cid;
 };
 
 export const retrieveFile = async (cid: string) => {
-  return ky(getLighthouseGatewayUrl(cid)).arrayBuffer();
+	return ky(getLighthouseGatewayUrl(cid)).arrayBuffer();
 };
