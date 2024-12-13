1ba026f28cc9b42cdc60351aafe88ec51d8de750
metalboyrick <rsulisthio@gmail.com>
2024-12-13 18:19:18 +0700

    fix(upload-attestation): use toaster and remove console log

--- apps/storybook/src/hooks/use-upload-attestation.ts
+++ apps/storybook/src/hooks/use-upload-attestation.ts
@@ -1,9 +1,9 @@
 import { getAttestationByUid } from "@/lib/eas/get-attestation-from-uid";
 import {
-	createLighthouseParams,
-	getLighthouseGatewayUrl,
-	uploadEncryptedFileWithText,
-	uploadText,
+  createLighthouseParams,
+  getLighthouseGatewayUrl,
+  uploadEncryptedFileWithText,
+  uploadText,
 } from "@/lib/filecoin/lighthouse/isomorphic";
 import { UploadAttestationParams } from "@/stories/attestations/attestations";
 import { useMutation } from "@tanstack/react-query";
@@ -11,79 +11,69 @@ import { useWalletClient } from "wagmi";
 import { useToast } from "./use-toast";
 
 const LIGHTHOUSE_API_KEY =
-	import.meta.env.VITE_LIGHTHOUSE_API_KEY ||
-	import.meta.env.LIGHTHOUSE_API_KEY! ||
-	import.meta.env.STORYBOOK_LIGHTHOUSE_API_KEY;
+  import.meta.env.VITE_LIGHTHOUSE_API_KEY ||
+  import.meta.env.LIGHTHOUSE_API_KEY! ||
+  import.meta.env.STORYBOOK_LIGHTHOUSE_API_KEY;
 
 export function useUploadAttestation() {
-	const { data: walletClient } = useWalletClient();
-	const { toast } = useToast();
+  const { data: walletClient } = useWalletClient();
+  const { toast } = useToast();
 
-	const mutation = useMutation({
-		mutationFn: async ({
-			uid,
-			payload,
-			chainId = 1,
-			isEncrypted,
-		}: UploadAttestationParams) => {
-			if (!walletClient) return;
+  const mutation = useMutation({
+    mutationFn: async ({
+      uid,
+      payload,
+      chainId = 1,
+      isEncrypted,
+    }: UploadAttestationParams) => {
+      if (!walletClient) return;
 
-			if ((uid || "").length > 0) {
-				const attestation = await getAttestationByUid(uid!, chainId);
-				if (!attestation.attestation) throw new Error("attestation not found");
-				payload = attestation;
-			}
+      if ((uid || "").length > 0) {
+        const attestation = await getAttestationByUid(uid!, chainId);
+        if (!attestation.attestation) throw new Error("attestation not found");
+        payload = attestation;
+      }
 
-			if (!payload) {
-				console.error(
-					"at least attestation payload or uid with chain must be present",
-				);
-				return;
-			}
-			// default to payload
-			const [apiKey, accountAddress, signedMessage] =
-				await createLighthouseParams({
-					account: walletClient.account,
-					options: { apiKey: LIGHTHOUSE_API_KEY },
-				});
+      if (!payload) {
+        toast({
+          title: "Error",
+          description:
+            "At least attestation payload or uid with chain must be present",
+        });
+        return;
+      }
+      // default to payload
+      const [apiKey, accountAddress, signedMessage] =
+        await createLighthouseParams({
+          account: walletClient.account,
+          options: { apiKey: LIGHTHOUSE_API_KEY },
+        });
 
-			const compiledPayload = { ...payload, chainId };
+      const compiledPayload = { ...payload, chainId };
 
-			if (isEncrypted) {
-				const { name, cid } = await uploadEncryptedFileWithText(
-					JSON.stringify(compiledPayload),
-					apiKey,
-					accountAddress,
-					signedMessage,
-				);
+      if (isEncrypted) {
+        const { name, cid } = await uploadEncryptedFileWithText(
+          JSON.stringify(compiledPayload),
+          apiKey,
+          accountAddress,
+          signedMessage,
+        );
+        toast({
+          title: `Encrypted Upload Successful for file : ${name}`,
+          description: getLighthouseGatewayUrl(cid),
+        });
+      } else {
+        const { name, cid } = await uploadText(
+          JSON.stringify(compiledPayload),
+          apiKey,
+        );
+        toast({
+          title: `Upload Successful for file : ${name}`,
+          description: getLighthouseGatewayUrl(cid),
+        });
+      }
+    },
+  });
 
-				console.log({
-					title: `Encrypted Upload Successful for file : ${name}`,
-					description: getLighthouseGatewayUrl(cid),
-				});
-
-				toast({
-					title: `Encrypted Upload Successful for file : ${name}`,
-					description: getLighthouseGatewayUrl(cid),
-				});
-			} else {
-				const { name, cid } = await uploadText(
-					JSON.stringify(compiledPayload),
-					apiKey,
-				);
-
-				console.log({
-					title: `Upload Successful for file : ${name}`,
-					description: getLighthouseGatewayUrl(cid),
-				});
-
-				toast({
-					title: `Upload Successful for file : ${name}`,
-					description: getLighthouseGatewayUrl(cid),
-				});
-			}
-		},
-	});
-
-	return { ...mutation };
+  return { ...mutation };
 }
