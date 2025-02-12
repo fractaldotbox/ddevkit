07489bff1f594359bf1b839750d53819d2cd9944
debuggingfuture <lauchunyin@debuggingfuture.com>
2025-02-13 02:36:12 +0800

    fix: clean up

--- apps/docs/astro.config.mjs
+++ apps/docs/astro.config.mjs
@@ -1,14 +1,14 @@
+import starlight from "@astrojs/starlight";
 // @ts-check
-import { defineConfig } from 'astro/config';
-import starlight from '@astrojs/starlight';
+import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
 	integrations: [
 		starlight({
-			title: 'Geist DappKit',
+			title: "Geist DappKit",
 			social: {
-				github: 'https://github.com/withastro/starlight',
+				github: "https://github.com/withastro/starlight",
 			},
 			// sidebar: [
 			// 	{
