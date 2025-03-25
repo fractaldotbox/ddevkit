4d19eaa7321a88dbd48fb56aa611ebbfb9992411
debuggingfuture <lauchunyin@debuggingfuture.com>
2025-03-26 02:36:30 +0800

    add: passport component

---
+++ packages/ui-react/src/lib/passport/api.int.test.ts
@@ -0,0 +1,43 @@
+import { describe, it, expect } from "vitest";
+import * as api from "./api";
+import { BY_USER } from "@geist/domain/user.fixture";
+
+// Test Ethereum address to use for testing
+const TEST_ADDRESS = BY_USER.vitalik.address;
+
+describe("Passport API Integration Tests", () => {
+	describe("getScoreWithAddress", () => {
+		it("should fetch a score for an address", async () => {
+			const result = await api.getScoreWithAddress(TEST_ADDRESS);
+
+			expect(result).toBeDefined();
+			expect(result.address).toBe(TEST_ADDRESS);
+			expect(typeof result.details.models.aggregate.score).toBe("number");
+		});
+	});
+
+	describe("getStampsMetadata", () => {
+		it("should fetch a list of stamps metadata", async () => {
+			const res = await api.getStampsMetadata();
+
+			expect(Array.isArray(res?.[0]?.groups?.[0]?.stamps)).toBe(true);
+			const stamp = res?.[0]?.groups?.[0]?.stamps?.[0];
+			expect(stamp).toHaveProperty("name");
+			expect(stamp).toHaveProperty("hash");
+		});
+	});
+
+	describe("getStampsByAddress", () => {
+		it("should fetch stamps for an address", async () => {
+			const result = await api.getStampsByAddress(TEST_ADDRESS, true);
+
+			expect(result).toBeDefined();
+			expect(Array.isArray(result.items)).toBe(true);
+
+			const stamp = result.items[0]!;
+			expect(stamp).toHaveProperty("credential");
+			expect(stamp?.metadata).toHaveProperty("name");
+			expect(stamp?.metadata).toHaveProperty("description");
+		});
+	});
+});
