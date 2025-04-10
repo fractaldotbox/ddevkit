d8ec3c26416b3fe99dabcb61c068b72c94a65a67
ken <kenyiu@users.noreply.github.com>
2025-04-10 18:05:51 +0800

    test: test for name stories

--- apps/storybook/src/stories/identity/Name.stories.tsx
+++ apps/storybook/src/stories/identity/Name.stories.tsx
@@ -1,3 +1,4 @@
+import { expect, within } from "@storybook/test";
 import { BY_USER } from "@geist/domain/user.fixture";
 import { Name } from "@geist/ui-react/components/identity/name";
 import type { Meta, StoryObj } from "@storybook/react";
@@ -20,4 +21,14 @@ export const Short: Story = {
 		addressOrEns: BY_USER.vitalik.address,
 	},
 	decorators: [withQueryClientProvider()],
+	play: async ({ canvasElement }) => {
+		const canvas = within(canvasElement);
+
+		// Simulate a delay to allow the name to load
+		await new Promise((resolve) => setTimeout(resolve, 1000));
+
+		// Check if the name is displayed
+		const name = await canvas.findByTestId("name");
+		expect(name).toBeInTheDocument();
+	},
 };
--- packages/ui-react/src/components/identity/name.tsx
+++ packages/ui-react/src/components/identity/name.tsx
@@ -4,5 +4,5 @@ import { useEnsData } from "#hooks/ens/use-efp-api";
 export const Name = ({ addressOrEns }: { addressOrEns: AddressOrEns }) => {
 	const { data, isSuccess } = useEnsData(addressOrEns);
 	const name = data?.ens?.name;
-	return <div>{name}</div>;
+	return <div data-testid="name">{name}</div>;
 };
