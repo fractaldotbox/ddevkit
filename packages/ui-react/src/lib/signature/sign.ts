cdb3857a2b9cef9e8a4cbff35b6d7d0ccefe8835
debuggingfuture <lauchunyin@debuggingfuture.com>
2025-04-11 02:56:04 +0800

    fix: sign lib

--- packages/ui-react/src/lib/signature/sign.ts
+++ packages/ui-react/src/lib/signature/sign.ts
@@ -1,10 +1,5 @@
 // ECDSA signatures over secp256k1
 
-import { sha256 } from "@noble/hashes/sha256";
-import { signMessage } from "@wagmi/core";
-import { useEffect, useRef, useState } from "react";
-import { useAccount, useConfig, useSignMessage } from "wagmi";
-
 import * as typed from "micro-eth-signer/typed-data";
 import { EIP712Domain, signTyped } from "micro-eth-signer/typed-data";
 export type Hex = `0x${string}`;
@@ -12,10 +7,6 @@ export type Hex = `0x${string}`;
 // https://www.npmjs.com/package/@noble/secp256k1
 // EIP-191
 export const signMessageRaw = async (privateKey: Hex, message: string) => {
-	console.log("signMessageRaw", privateKey, message);
-	// const msgHash = sha256(message);
-	// sha256 of 'hello world'
-
 	const signature = typed.personal.sign(message, privateKey);
 	return signature as Hex;
 };
