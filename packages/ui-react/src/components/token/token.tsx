de23b59e1252af432a41d387289266752e390e49
ken <kenyiu@users.noreply.github.com>
2025-04-10 14:54:01 +0800

    fix: include chainId in token info fetch for ERC20 functions

--- packages/ui-react/src/components/token/token.tsx
+++ packages/ui-react/src/components/token/token.tsx
@@ -172,21 +172,25 @@ export const useTokenInfo = ({
 			allowFailure: false,
 			contracts: [
 				{
+					chainId,
 					address,
 					abi: erc20Abi,
 					functionName: "decimals",
 				},
 				{
+					chainId,
 					address,
 					abi: erc20Abi,
 					functionName: "name",
 				},
 				{
+					chainId,
 					address,
 					abi: erc20Abi,
 					functionName: "symbol",
 				},
 				{
+					chainId,
 					address,
 					abi: erc20Abi,
 					functionName: "totalSupply",
