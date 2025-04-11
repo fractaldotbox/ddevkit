e36d75f9bfb977b227ee2d5f1a466b1e1b6b80ac
ken <kenyiu@users.noreply.github.com>
2025-04-11 16:36:15 +0800

    test: add testCodecForm utility function for CodecForm stories

--- apps/storybook/src/stories/codec/CodecForm.stories.tsx
+++ apps/storybook/src/stories/codec/CodecForm.stories.tsx
@@ -1,4 +1,6 @@
 import { CodecForm } from "@geist/ui-react/components/codec/CodecForm";
+import { setupCanvas } from "../utils/test-utils";
+import { userEvent, within, expect } from "@storybook/test";
 import type { Meta, StoryObj } from "@storybook/react";
 
 import { map } from "nanostores";
@@ -17,12 +19,34 @@ const meta = {
 export default meta;
 type Story = StoryObj<typeof meta>;
 
+async function testCodecForm(canvasElement: any, expectedTexts: string[]) {
+	const { canvas } = await setupCanvas(canvasElement, 0);
+	const input = await canvas.getByRole("textbox");
+	await expect(input).toBeVisible();
+	await userEvent.type(input, "123abc");
+	await new Promise((resolve) => setTimeout(resolve, 1000));
+	await expect(canvas.getByText('{"message":"123abc"}')).toBeVisible();
+	await expect(canvas.getByText("codec: cid")).toBeVisible();
+	expectedTexts.forEach(async (text) => {
+		await expect(canvas.getByText(text)).toBeVisible();
+	});
+}
+
 export const Multibase64: Story = {
 	args: {
 		$codec: map({
 			input: "",
 			codec: "multibase64",
 		}),
+	},
+	play: async ({ canvasElement }) => {
+		await testCodecForm(canvasElement, [
+			"bagaaieramt5v6v252z63hzr2l7f37gwznp7s5ialbtvasyfno4iz3sbjkstq",
+			"codec: multibase64",
+			"mAYAEEiBk+19XXdZ9s+Y6X8u/mtlr/y6gCwzqCWCtdxGdyClUpw",
+			"codec: base64",
+			"AYAEEiCcptlVPp19Q9ncKA2+CSUrhHCKGAlJrVnYuV0NVfUE4w==",
+		]);
 	},
 };
 
@@ -32,6 +56,15 @@ export const Multibase16: Story = {
 			input: "",
 			codec: "multibase16",
 		}),
+	},
+	play: async ({ canvasElement }) => {
+		await testCodecForm(canvasElement, [
+			"bagaaieramt5v6v252z63hzr2l7f37gwznp7s5ialbtvasyfno4iz3sbjkstq",
+			"codec: multibase16",
+			"f018004122064fb5f575dd67db3e63a5fcbbf9ad96bff2ea00b0cea0960ad77119dc82954a7",
+			"codec: base16",
+			"01800412209CA6D9553E9D7D43D9DC280DBE09252B84708A180949AD59D8B95D0D55F504E3",
+		]);
 	},
 };
 
