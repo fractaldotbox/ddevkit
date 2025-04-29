17448aa782561152ef1b25198ababab95a7994d7
debuggingfuture <lauchunyin@debuggingfuture.com>
2025-04-29 15:50:35 +0900

    fix: upload form paths

--- apps/storybook/src/stories/filecoin/UploadDropzone.stories.tsx
+++ apps/storybook/src/stories/filecoin/UploadDropzone.stories.tsx
@@ -1,9 +1,10 @@
 import type { Meta, StoryObj } from "@storybook/react";
 
 import config from "@geist/domain/config";
+import UploadDropzone from "@geist/ui-react/components/filecoin/UploadDropzone";
 import { uploadFiles } from "@geist/ui-react/lib/filecoin/lighthouse/browser";
 import { withToaster } from "../decorators/toaster";
-import UploadDropzone from "./UploadDropzone";
+
 import { uploadSuccessToast } from "./upload-toast";
 
 const meta = {
